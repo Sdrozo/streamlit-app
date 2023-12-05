a5bd1989bae763c9eaab8049e769d80567083254
sdrozo <sebastiandrozo@gmail.com>
2023-12-05 21:31:55 +0000

    Second set of changes added second graph

--- src/app.py
+++ src/app.py
@@ -1,10 +1,8 @@
 # Streamlit live coding script
 import streamlit as st
 import pandas as pd
-import matplotlib.pyplot as plt
 import plotly.express as px
 import plotly.graph_objects as go
-from urllib.request import urlopen
 import json
 from copy import deepcopy
 
@@ -63,7 +61,7 @@ st.subheader('Internet Users Over Time Comparison')
 
 
 # Widgets: selectbox
-Entities = sorted(pd.unique(internet_df['Entity']))
+Entities = pd.unique(internet_df['Entity'])
 Entity1 = middle_column.selectbox("Choose the first country", Entities)
 Entity2 = right_column.selectbox("Choose the second country", Entities)
 
